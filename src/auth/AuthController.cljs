2e71b40af6dbf5ceed95cb101f41d80fc7dbe9c6
Maxim Mitroshin <rocco66max@gmail.com>
2013-12-17 10:00:35 +0400

    Fix firebase twitter login

--- src/auth/AuthController.cljs
+++ src/auth/AuthController.cljs
@@ -2,8 +2,14 @@
   (:use [gost :only (gost)]))
 
 (.controller gost "AuthController" (fn [$scope go-base $firebaseAuth]
-    (.initialize $firebaseAuth (.root go-base)
-                                 (js-obj "scope" $scope "name" "user"))
-    (set! (.-login-twitter $scope) #(.login $firebaseAuth "twitter"))
-    (set! (.-logout $scope) #(.logout $firebaseAuth))
+    ;; (.initialize $firebaseAuth (.root go-base)
+    ;;                              (js-obj "scope" $scope "name" "user"))
+    (def auth ($firebaseAuth (.root go-base)))
+    (defn login-twitter [] (do
+                             (def login-promise (.$login auth "twitter"))
+                             (.then login-promise #(set! (.-user $scope) %))
+                             ))
+    (set! (.-login-twitter $scope) login-twitter)
+    (set! (.-logout $scope) #(.$logout auth))
+    ;; (set! (.-user $scope) #(.$logout auth))
 ))
